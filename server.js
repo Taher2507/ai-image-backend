79eda3313bfc6ec21d8d779cf15aa17602bf8fd5
Taher2507 <tbalasinorwala@smartqc.io>
2024-10-16 21:36:19 +0530

    server.js fixed

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const imageRoutes = require('./routes/imageRoutes');
 
 // Use Routes
 app.use(`/users`, userRoutes); // For user-related routes
-app.use(`${BASE_URL}images`, imageRoutes); // For image-related routes
+app.use(`/images`, imageRoutes); // For image-related routes
 
 // Base Route to check if the server is running
-app.get(`${BASE_URL}`, (req, res) => {
+app.get(`/`, (req, res) => {
     res.send("Server is Running...");
 });
 
