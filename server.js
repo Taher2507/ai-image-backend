3186b5b7fa73b2442ea83ea707449c907b77448b
Taher2507 <tbalasinorwala@smartqc.io>
2024-10-16 21:29:05 +0530

    server.js fixed

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const userRoutes = require('./routes/userRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
 // Use Routes
-app.use(`${BASE_URL}/users`, userRoutes); // For user-related routes
-app.use(`${BASE_URL}/images`, imageRoutes); // For image-related routes
+app.use(`/users`, userRoutes); // For user-related routes
+app.use(`${BASE_URL}images`, imageRoutes); // For image-related routes
 
 // Base Route to check if the server is running
 app.get(`${BASE_URL}`, (req, res) => {
@@ -32,7 +32,7 @@ app.use((req, res) => {
 });
 
 // For Render (binding to a port)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
